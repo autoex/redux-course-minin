55b087a5031e4da4919bc21a4df9c53d8d967d48
autoex <autoex@gmail.com>
2021-06-28 13:53:48 +0300

    add grid for components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import FormPosts from "./components/FormPosts";
+import Posts from "./components/Posts";
+import FetchedPosts from "./components/FetchedPosts";
 
 const App = () => {
-  return (
-      <div className={'container'}>
-        ddd
-      </div>
-  );
+    return (
+        <div className='container pt-3'>
+            <div className='row'>
+                <div className='col'><FormPosts /></div>
+            </div>
+            <div className='row'>
+                <div className='col'><Posts/></div>
+                <div className='col'><FetchedPosts/></div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
