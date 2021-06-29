8afb30baedef18300b3631899024921e9dd9bb57
autoex <autoex@gmail.com>
2021-06-29 17:21:50 +0300

    replace connect HOC on hooks

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import Post from "./Post";
-import {connect} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {getPosts} from "../redux/posts-reducer";
 
-const Posts = ({posts,isFetching, getPosts}) => {
+export default () => {
+
+    const isFetching = useSelector(state => state.posts.isFetching )
+    const posts = useSelector(state => state.posts.fetchedPosts )
+    const dispatch = useDispatch()
 
     if (!posts.length) return (<div className='text-center'>
-       {isFetching ? 'Loading' : <button className='btn btn-primary' onClick={getPosts}>Load</button>}
+       {isFetching ? 'Loading...' : <button className='btn btn-primary' onClick={()=>dispatch(getPosts())}>Load</button>}
     </div>)
 
     return posts.map((post, idx) => <Post post={post} key={idx}/>)
 
 };
 
-const mapStateToProps =(state)=> ({
-    posts: state.posts.fetchedPosts,
-    isFetching: state.posts.isFetching
 
-});
 
-export default connect(mapStateToProps, {getPosts})(Posts);
